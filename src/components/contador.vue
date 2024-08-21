792e843ca7f8b4aecc0b60e556e52ae28d08c964
Emmanuel Vital <vytal.0410@gmail.com>
2024-08-21 13:23:50 -0600

    Creción de contadores

---
+++ src/components/contador.vue
@@ -0,0 +1,26 @@
+<template>
+<h1>Hello World</h1>
+<p>{{ contador }}</p>
+<button @click="incrementar">Incrementar</button>
+<button @click="decrementar">Decrementar</button>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+const contador = ref(0);
+//Funcion para incrementar
+const incrementar = () =>{
+  contador.value++
+}
+//Funcion para decrementar
+const decrementar = () =>{
+  contador.value--
+}
+</script>
+
+<style scoped>
+p {
+  color: red;
+  }
+</style>
+
